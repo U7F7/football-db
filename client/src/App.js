0b875509bc01840a37d7fa139c269ea79b9168fc
phong21 <phong21@students.cs.ubc.ca>
2023-11-01 19:48:26 -0700

    Connected backend to frontend
    implemented api to get whole tables from oracle and a helper function to convert that to standard json formatting.
    
    have a test in the front end with react display the raw json from the get request

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import axios from "axios";
+import { React, useState, useEffect } from "react";
 
 const App = () => {
+	const [data, setData] = useState({});
+
+	useEffect(() => {
+		axios.get("http://localhost:65535/table/Awards")
+			.then((res) => {
+				setData(res.data);
+			})
+			.catch(err => console.error(err));
+	}, [data]);
+
+	// const data = JSON.stringify({a: "1"});
+
 	return (
 		<div className="App">
 			<h1>Hello World</h1>
+			<p>{data ? JSON.stringify(data) : "Loading..."}</p>
 		</div>
 	);
 };
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const loadEnvFile = require("./utils/envUtil");
 const envVariables = loadEnvFile("../.env");
 const oracledb = require("oracledb");
+const queryToJson = require("./utils/helpers");
 
 const app = express();
 const PORT = envVariables.PORT || 65534; // Adjust the PORT if needed (e.g., if you encounter a "port already occupied" error)
@@ -23,9 +24,75 @@ app.listen(PORT, () => {
 
 // Middleware setup
 app.use(express.json()); // Parse incoming JSON payloads
-
-app.get("/", (req, res) => {
-	res.json({hi: "hello world"});
+// CORS fix
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
 });
 
 
+app.get("/test", (req, res) => {
+	res.json({hi: "hello world"});
+});
+
+// Wrapper to manage OracleDB actions, simplifying connection handling.
+const withOracleDB = (action) => {
+	let connection;
+
+	return oracledb.getConnection(DB_CONFIG)
+		.then((conn) => {
+			connection = conn;
+			return action(connection)
+				.then((result) => {
+					return result;
+				})
+				.catch((err) => {
+					console.error(err);
+					throw err;
+				});
+		})
+		.catch((err) => {
+			console.error(err);
+			throw err;
+		})
+		.finally(() => {
+			if (connection) {
+                return connection.close()
+                    .catch((err) => {
+                        console.error(err);
+                    });
+            }
+		});
+}
+
+async function testOracleConnection() {
+    return await withOracleDB(async (connection) => {
+		console.log("Oracle connection success!");
+        return true;
+    }).catch(() => {
+		console.log("Oracle connection failed!");
+        return false;
+    });
+}
+
+testOracleConnection();
+
+const getTable = (table) => {
+    return withOracleDB((connection) => {
+        return connection.execute(`SELECT * FROM ${table}`)
+            .then((result) => {
+                return result;
+            })
+            .catch(() => {
+                return [];
+            });
+    });
+}
+
+app.get("/table/:table", async (req, res) => {
+	const result = await getTable(req.params.table);
+	const toJson = queryToJson(result);
+	return res.json(toJson);
+});
