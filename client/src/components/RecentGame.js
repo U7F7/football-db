016ea1662f6954bc8dacead12b243ca4c6068347
phong21 <phong21@students.cs.ubc.ca>
2023-11-20 12:59:13 -0800

    RecenGames styling

--- client/src/components/RecentGame.js
+++ client/src/components/RecentGame.js
@@ -12,25 +12,31 @@ const RecentGame = ({ date, home, homeScore, away, awayScore }) => {
 		padding: 0
 	};
 
+	const teamScoreStyle = { 
+		display: "flex", 
+		justifyContent: "right", 
+		padding: 0
+	};
+
 	return (
 		<Card>
 			<Card.Body>
 				<Card.Title>{date}</Card.Title>
 				<Container>
 					<Row>
-						<Col xs={10} style={teamNameStyle}>
+						<Col xs="auto" style={teamNameStyle}>
 							<p style={{ marginBottom: 0 }}>{home}</p>
 						</Col>
-						<Col xs={2}>
-							{homeScore}
+						<Col style={teamScoreStyle}>
+							<p style={{ marginBottom: 0 }}>{homeScore}</p>
 						</Col>
 					</Row>
 					<Row>
-						<Col xs={10} style={teamNameStyle}>
+						<Col xs="auto" style={teamNameStyle}>
 							<p style={{ marginBottom: 0 }}>{away}</p>
 						</Col>
-						<Col xs={2}>
-							{awayScore}
+						<Col style={teamScoreStyle}>
+							<p style={{ marginBottom: 0 }}>{awayScore}</p>
 						</Col>
 					</Row>
 				</Container>
