1807f6cbd92bf4df712de830ee100675cc271eb6
phong21 <phong21@students.cs.ubc.ca>
2023-11-27 01:47:38 -0800

    Update init.sql
    Updated stats to be more realistic

--- client/src/components/games/Games.js
+++ client/src/components/games/Games.js
@@ -2,7 +2,6 @@ import { React, useState, useEffect } from "react";
 
 import Game from "./Game";
 
-import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
